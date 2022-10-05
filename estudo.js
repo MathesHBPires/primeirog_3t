d641875b7c2fee69d2ecea6b58267e14c1c1757f
MathesHBPires <108944938+MathesHBPires@users.noreply.github.com>
2022-10-05 11:56:27 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -5,3 +5,22 @@ function escreva(){
     document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
     document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
 }
+
+let lista = ["Terra", "Marte", "Saturno", "Jupiter","Venus"];
+
+function mostraLista (){
+    document.write("Tamanho da lista:" + lista.length + "<br>")
+    document.write("Planeta" + lista[4])
+    for(let i = 0; i < 5; i++){
+        document.write("Planeta" + lista[i] + "<br>");
+    }
+} 
+
+function repete (){
+    for(let i = 1; i <= 10; i++){
+        document.write("Tabuada do  = " + i + "<br>");
+        for(let j = 1; j <=10; j++){
+            document.write(i+ " x " + j + " = "+(i*j)+"<br>")
+        }
+    }
+}
