888d1421e8af345443697fc1e70eb5cc94f58f89
MathesHBPires <108944938+MathesHBPires@users.noreply.github.com>
2022-10-07 12:48:31 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,26 +1,13 @@
-let tabuada = 2;
-
-function escreva(){
-    document.write(" Taboada do" + tabuada + " <br>")
-    document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
-}
-
-let lista = ["Terra", "Marte", "Saturno", "Jupiter","Venus"];
-
-function mostraLista (){
-    document.write("Tamanho da lista:" + lista.length + "<br>")
-    document.write("Planeta" + lista[4])
-    for(let i = 0; i < 5; i++){
-        document.write("Planeta" + lista[i] + "<br>");
+function total(){
+    let c = document.getElementById("capital").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let m = 0;
+    for(let i = 1; i <= t; i++){
+         m = c * (1 + (j/100));
+         document.write("Valor no mês " + i  + " = " + m + "<br>");
+         c = m;
     }
-} 
-
-function repete (){
-    for(let i = 1; i <= 10; i++){
-        document.write("Tabuada do  = " + i + "<br>");
-        for(let j = 1; j <=10; j++){
-            document.write(i+ " x " + j + " = "+(i*j)+"<br>")
-        }
-    }
+    
+    document.write("Montante:" + m);
 }
