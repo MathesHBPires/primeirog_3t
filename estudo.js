5549486eb094d6e793b8f4533042e15972733ee8
MathesHBPires <108944938+MathesHBPires@users.noreply.github.com>
2022-11-11 12:47:03 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,78 +1,87 @@
-function moeda(atual) {
-  return atual.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
-}
-function total() {
-  let c = document.getElementById("capital").value;
-  let j = document.getElementById("juros").value;
-  let t = document.getElementById("meses").value;
-  if (!Number(c)) {
-    alert("O valor do capital deve ser um número.");
-    document.getElementById("capital").value = "";
-    document.getElementById("capital").focus();
-    return
-  }
-  if (!Number(j)) {
-    alert("O valor dos juros deve ser um número.");
-    document.getElementById("juros").value = "";
-    document.getElementById("juros").focus();
-    return
-  }
-  if (!Number(t)) {
-    alert("A quantidade de meses deve ser um número.");
-    document.getElementById("meses").value = "";
-    document.getElementById("meses").focus();
-    return
-  }
-  let m = 0;
-  let texto = "";
-  for (let i = 1; i <= t; i++) {
-    m = c * (1 + (j / 100));
-    texto += i + " : " + moeda(m) + "<br>"
-    //document.write("Valor no mês " + i + " = " + m + "<br>");
-    c = m;
-  }
-  document.getElementById("ListaMes").innerHT = texto
-  document.getElementById("total").innerHT = moedaML(m);
-  //document.write("Montante:" + m);
-}
-function soma() {
-  let val1 = document.getElementById("v1").value;
-  let val2 = document.getElementById("v2").value;
-  let r = Number(val1) + Number(val2);
-  document.getElementById(" resultado ").innerHTML = r;
+<!DOCTYPE html>
+<html lang="pt-br">
 
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro G</title>
+    <script src="estudo.js"></script>
+    <link rel="stylesheet" href="geral.css">
+</head>
 
+<body>
+    <input type="checkbox" id="input-calcFin">
 
+    <div class="calc">
+        <label for="input-calcFin">
+            <h3>Financeiro</h3>
+        </label>
+        <div class="calcF">
+            <div id="spTotal">
+                <span id="total">R$: 0,00</span>
+            </div>
+            Capital: <input id="capital" placeholder="R$ ??" type="text"><br>
+            Juros mensais: <input id="juros" placeholder="% ??" type="text"><br>
+            Quantidade de meses: <input id="meses" placeholder="Tempo ??" type="text"><br>
+            <button onclick="total()">Calcular</button>
+            <div id="spLista">
+                <span id="listaMes"></span>
+            </div>
+        </div>
+    </div>
+</body>
 
-function subtracao() {
-  let val1 = document.getElementById("v1").value;
-  let val2 = document.getElementById("v2").value;
-  let r = Number(val1) - Number(val2);
-  document.getElementById("resultado").innerHTML = r;
-}
+</html>
 
-function divisao() {
-  let val1 = document.getElementById("v1").value;
-  let val2 = document.getElementById("v2").value;
-  let r = Number(val1) / Number(val2);
-  document.getElementById("resultado").innerHTML = r;
-}
+<!DOCTYPE html>
+<html lang="pt-br">
 
-function multiplicacao() {
-  let val1 = document.getElementById("v1").value;
-  let val2 = document.getElementById("v2").value;
-  let r = Number(val1) * Number(val2);
-  document.getElementById("resultado").innerHTML = r;
-}
-function porcentagem() {
-  let val1 = document.getElementById("v1").value;
-  let val2 = document.getElementById("v2").value;
-  let c = 100
-  let p = Number(val2) / Number(c)
-  let r = Number(val1) * Number(p);
-  document.getElementById("resultado").innerHTML = r;
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro G</title>
+    <script src="estudo.js"></script>
+</head>
+
+<body>
 
 
+    Capital: <input id="valor" type="text"> <br>
+    Juros Mensais: <input id="juros" type="text"> <br>
+    Nr. de Meses: <input id="meses" type="text"> <br>
+    <button onclick="total()">Calcula</button>
 
+    <br><br><br>
+    <span id="resultado"></span><br><br>
+    valor 1: <input id="v1" type="text"><br>
+    valor 2: <input id="v2" type="text"><br>
+    <button onclick="adicao()">Soma</button>
+    <button onclick="subtracao()">Subtração</button>
+    <button onclick="divisao()">Divisão</button>
+    <button onclick="multiplicacao()">Multiplicação</button>
+    <button onclick="porcentagem()">Porcentagem</button>
+</body>
+
+</html>
+
+ <input type="checkbox" id="input-calcesg">
+
+    <div class="calcE">
+        <label for="input-calcESG">
+            <h3>Equação Segundo Grau</h3>
+        </label>
+        <div class="calcEq">
+            <div id="spRaiz">
+                <span id="raiz">x1:? e x2:?</span>
+            </div>
+            <input id="a" placeholder="a" type="text"><br>
+            <input id="b" placeholder="b" type="text"><br>
+            <input id="c" placeholder="c" type="text"><br>
+            <button onclick="calculaRaiz()">Calcular</button>
+            <div id="spLista">
+                <span id="listaMes"></span>
+            
+        </div>
+    </div>
